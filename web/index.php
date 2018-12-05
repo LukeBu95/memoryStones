43c5b3cd69bc5d9c5589eb0a21a626640adbf96f
Lukas Buttke <lukas.buttke@drive.eu>
2018-12-05 17:27:22 +0100

    navigation fix

--- web/index.php
+++ web/index.php
@@ -16,59 +16,73 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-12">
-			<header> HEADER </header>
+			<header>
+				<h2 style="font-weight: bold">STOLPERSTEINE</h2>
+				<h4 style="font-weight: bold">Ein Mahnmal für die Opfer des 3. Reichs</h4>
+			</header>
 		</div>
 	</div>
 	<div class="row">
-		<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
-			<a class="navbar-brand" href="index.php"> MEMORY STONES</a>
-			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggler="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle Navigation">
-				<span class="navbar-toggler-icon"></span></button>
-			<div class="collapse navbar-collapse" id="navbarText">
-				<ul class="nav navbar-nav mr-auto">
-					<li class="nav-item">
-						<a class="nav-link" href="#"> START</a>
-					</li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About </a>
-						<ul class="dropdown-menu bg-secondary">
-							<li class="nav-item">
-								<a class="nav-link" href="Geschichte.html">Über das Projekt</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="gunter.html">Über Gunter Demnig</a>
-							</li>
-						</ul>
-					</li>
-					<li class="dropdown navbar-dark bg-secondary">
-						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Stolpersteine
-						</a>
-						<ul class="dropdown-menu bg-secondary">
-							<li class="nav-item">
-								<a class="nav-link" href="Stolpersteine_Hannover.html">Hannover</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link">Stolperstein Guide</a>
-							</li>
-						</ul>
-					</li>
-					<li class="nav-item"><a class="nav-link" href="Website/link.html">Downloads</a></li>
-				</ul>
-				<span class="navbar-text">&copy; 2018</span>
+		<div class="col-md-12">
+			<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+				<a class="navbar-brand" href=""> MEMORY STONES</a>
+				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggler="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle Navigation">
+					<span class="navbar-toggler-icon"></span></button>
+
+				<div class="collapse navbar-collapse" id="navbarText">
+					<ul class="nav navbar-nav mr-auto">
+						<li class="nav-item mr-4">
+							<a class="nav-link" href="#"> &nbsp; </a>
+						</li>
+
+						<li class="dropdown mr-4">
+							<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About
+								<span class="caret"></span></a>
+							<ul class="dropdown-menu bg-secondary">
+								<li class="nav-item">
+									<a class="nav-link" href="Geschichte.html">Über das Projekt</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="gunter.html">Über Gunter Demnig</a>
+								</li>
+							</ul>
+						</li>
+						<li class="dropdown mr-4">
+							<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Stolpersteine
+								<span class="caret"></span></a>
+							<ul class="dropdown-menu bg-secondary">
+								<li class="nav-item">
+									<a class="nav-link" href="Stolpersteine_Hannover.html">Hannover</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link">Stolperstein Guide</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item mr-4"><a class="nav-link" href="link.html">Downloads</a></li>
+					</ul>
+					<span class="navbar-text">&copy; 2018</span>
+				</div>
+			</nav>
+		</div>
+
+
+		<div class="col-md-12">
+			<!-- Content -->
+			<div class="container">
+				<?php
+				 include "startup.html";
+				?>
 			</div>
-		</nav>
-	</div>
-	<div class="col-md-12">
-		<!-- Content -->
-	</div>
-	<div class="col-md-12">
-		<footer> footer</footer>
+		</div>
+		<div class="col-md-12">
+			<footer> School Project for the MMBBS IT Course</footer>
+		</div>
+
 	</div>
 
-</div>
-
-<script src="js/jquery-3.1.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-<script src="js/bootstrap/bootstrap.js"></script>
+	<script src="js/jquery-3.1.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="js/bootstrap/bootstrap.js"></script>
 </body>
 </html>
