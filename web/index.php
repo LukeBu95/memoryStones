a89ca4c8046477fba9b9ae406d4a9476204eeaac
Lukas Buttke <lukas.buttke@drive.eu>
2018-12-05 15:31:27 +0100

    new bootstrap nav

---
+++ web/index.php
@@ -0,0 +1,74 @@
+<!-- Bootstrap 4 Tutorial von www.bootstrapworld.de -->
+<!-- ============================================== -->
+<html lang="de">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS laden -->
+	<link rel="stylesheet" href="css/bootstrap/bootstrap.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+</head>
+<body>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<header> HEADER </header>
+		</div>
+	</div>
+	<div class="row">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+			<a class="navbar-brand" href="index.php"> MEMORY STONES</a>
+			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggler="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle Navigation">
+				<span class="navbar-toggler-icon"></span></button>
+			<div class="collapse navbar-collapse" id="navbarText">
+				<ul class="nav navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#"> START</a>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About </a>
+						<ul class="dropdown-menu bg-secondary">
+							<li class="nav-item">
+								<a class="nav-link" href="Geschichte.html">Über das Projekt</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="gunter.html">Über Gunter Demnig</a>
+							</li>
+						</ul>
+					</li>
+					<li class="dropdown navbar-dark bg-secondary">
+						<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Stolpersteine
+						</a>
+						<ul class="dropdown-menu bg-secondary">
+							<li class="nav-item">
+								<a class="nav-link" href="Stolpersteine_Hannover.html">Hannover</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link">Stolperstein Guide</a>
+							</li>
+						</ul>
+					</li>
+					<li class="nav-item"><a class="nav-link" href="Website/link.html">Downloads</a></li>
+				</ul>
+				<span class="navbar-text">&copy; 2018</span>
+			</div>
+		</nav>
+	</div>
+	<div class="col-md-12">
+		<!-- Content -->
+	</div>
+	<div class="col-md-12">
+		<footer> footer</footer>
+	</div>
+
+</div>
+
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="js/bootstrap/bootstrap.js"></script>
+</body>
+</html>
